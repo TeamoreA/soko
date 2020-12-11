138d4d6fb5fc639363dc30bce663d7ae9b224c43
Timothy Kamau <tim@Timothys-MacBook-Pro.local>
2020-12-11 08:43:22 +0300

    add readme file - add a detailed readme on how to setup and test the app - add api docs to the readme - clean files and imports

--- helpers/renderers.py
+++ helpers/renderers.py
@@ -4,7 +4,7 @@ from typing import Any, Dict, List, Optional, Union
 from rest_framework import status
 from rest_framework.exceptions import ErrorDetail
 from rest_framework.renderers import JSONRenderer
-from rest_framework.utils.serializer_helpers import ReturnDict, ReturnList
+from rest_framework.utils.serializer_helpers import ReturnDict
 
 message_map = lambda view: lambda status_code: {  # noqa
     "POST": "%s created successfully" % view.name.capitalize(),
@@ -47,45 +47,3 @@ class UserJSONRenderer(JSONRenderer):
             )
         )
 
-
-class DefaultRenderer(JSONRenderer):
-    """
-    custom renderer class for application views
-    """
-
-    def render(
-        self,
-        data: Union[Dict[str, Union[str, List[ErrorDetail]]], ReturnList],
-        accepted_media_type: Optional[str] = None,
-        renderer_context: Optional[Dict[str, Any]] = None,
-    ) -> bytes:
-        """
-        format response data for application views
-        """
-        if renderer_context["response"].status_code == 422:
-            renderer_context["response"].status_code = status.HTTP_400_BAD_REQUEST
-        status_code = renderer_context["response"].status_code
-        if renderer_context["request"].method == "GET":
-            if renderer_context["kwargs"] == {}:
-                message = (
-                    "All %s"  # TODO fix bug for single and all orders views
-                    % renderer_context["view"].pluralized_name.capitalize()
-                )
-            else:
-                message = "%s info" % renderer_context["view"].name.capitalize()
-        else:
-            message = message_map(renderer_context["view"])(
-                renderer_context["request"].method
-            )
-        data = (
-            ({"status": "success", "message": message, "data": data})
-            if status.is_success(status_code)
-            else {
-                "status": "error",
-                "message": "Correct the errors below",
-                "data": data,
-            }
-        )
-        return super(DefaultRenderer, self).render(
-            data, accepted_media_type, renderer_context
-        )
